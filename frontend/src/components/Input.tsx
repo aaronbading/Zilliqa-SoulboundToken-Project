9973da33eb8525f9498f1fff85fd2f0db8947dbf
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-06-13 22:28:26 +0800

    add Button and Input component

---
+++ frontend/src/components/Input.tsx
@@ -0,0 +1,32 @@
+import cn from "classnames";
+import React from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = (props: InputProps) => {
+  return (
+    <input
+        className={cn(
+          "form-control",
+          "px-3",
+          "py-1.5",
+          "text-base",
+          "font-normal",
+          "text-gray-700",
+          "bg-white bg-clip-padding",
+          "border border-solid border-gray-300",
+          "rounded",
+          "transition",
+          "ease-in-out",
+          "m-0",
+          "focus:text-gray-700",
+          "focus:bg-white",
+          "focus:border-blue-600",
+          "focus:outline-none"
+        )}
+        {...props}
+      />
+  )
+}
+
+export default Input;
