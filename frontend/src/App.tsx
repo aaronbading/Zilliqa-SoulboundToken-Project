dd50e66fdeaccfd33b9e8454ebc904f9057a274a
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-06-17 21:27:40 +0800

    add landing and container page to routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,23 @@
 import { Routes, Route } from "react-router-dom";
+import AppContainer from "./components/AppContainer";
 import Dummy from "./Dummy";
+import Landing from "./pages/Landing";
 import WalletProvider from "./providers/walletProvider";
 import ZilliqaProvider from "./providers/ZilliqaProvider";
 
 const App = () => {
-
   return (
-    <div>
-      <WalletProvider>
-        <ZilliqaProvider>
-          <Routes>
-            <Route path="/" element={<Dummy />} />
-          </Routes>
-        </ZilliqaProvider>
-      </WalletProvider>
-    </div>
+    <WalletProvider>
+      <ZilliqaProvider>
+        <Routes>
+          <Route element={<AppContainer />} path="/">
+            <Route path="/" element={<Landing />} />
+            <Route path="/dummy" element={<Dummy />} />
+          </Route>
+        </Routes>
+      </ZilliqaProvider>
+    </WalletProvider>
   );
-}
+};
 
 export default App;
