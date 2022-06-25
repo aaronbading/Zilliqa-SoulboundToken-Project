fece21b7f8a131138810db89d988ec40727064c1
aaronbading <aaronbading.ab@gmail.com>
2022-06-24 22:57:35 -0500

    Add some content

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -9,10 +9,41 @@ export default function Landing() {
           <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium text-gray-900 dark:text-white">
             Zilliqua Soul-Bound Token
           </h1>
-          <p className="mb-8 leading-relaxed">Put some information</p>
-          <p className="mb-8 leading-relaxed">Put some information</p>
+          <h2 className="title-font sm:text-1xl text-3xl mb-4 font-medium text-gray-900 dark:text-white my-12">
+            What is an SBT ?
+          </h2>
+          <p className="mb-8 leading-relaxed">
+            The Zilliqa Soul-Bound Token is intended to be a proof-of-concept
+            utility token that allows users to own their on-chain identity. At
+            its core, it is non-transferable token that users mint for
+            themselves. This has been implemented on the Zilliqa blockchain
+            using the native smart-contract language Scilla.
+          </p>
+          <h2 className="title-font sm:text-1xl text-3xl mb-4 font-medium text-gray-900 dark:text-white">
+            What is its purpose ?
+          </h2>
+          <p className="mb-8 leading-relaxed">
+            The token’s purpose is to bring utility by incentivizing users to
+            interact with the underlying platform to grow their SBT Profile. An
+            example of user incentivization is earning educational achievements
+            for completing a course or solving a problem. Rewards for
+            participation could theoretically be in the forms of NFTs, digital
+            assets or just a simple “kudos” on the Profile Page. This system
+            establishes a sense of reputation and trust in profiles which is
+            essential for thriving online communities.
+          </p>
+          <h2 className="title-font sm:text-1xl text-3xl mb-4 font-medium text-gray-900 dark:text-white">
+            What is the vision of SBTs?
+          </h2>
+          <p className="mb-8 leading-relaxed">
+            The concept itself of an SBT is broad in nature and quite novel.
+            Thus, the vision of project reaches far beyond what has been
+            currently implemented. Nevertheless, this project serves as one of
+            many starting points for powerful idea that will enable another
+            layer of incentivization on top of decentralized digital ownership.
+          </p>
           <div className="flex justify-center">
-            <Link to="/create-profile">Button</Link>
+            <Link to="/create-profile">Minting Page</Link>
           </div>
         </div>
         <div className="rounded lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
