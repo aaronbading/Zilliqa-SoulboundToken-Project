755d671f19ef0db69e3294faa13b8a64cb923fa2
aaronbading <aaronbading.ab@gmail.com>
2022-06-28 15:51:21 -0500

    Merge

--- frontend/src/components/AppContainer.tsx
+++ frontend/src/components/AppContainer.tsx
@@ -1,16 +1,12 @@
-import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
-import cn from 'classnames';
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import cn from "classnames";
 
 export default function AppContainer() {
   return (
     <div>
       <Navbar />
-<<<<<<< HEAD
-      <div className={cn()}>
-=======
-      <div className={cn('')}>
->>>>>>> 2e08fa8455f0c65a1c91975979f26d9929cf8267
+      <div className={cn("")}>
         <Outlet />
       </div>
     </div>
