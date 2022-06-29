a1516f3aa4219130e0e3058d76c25831adb4b7a9
John Montejano <johnmontejano2@gmail.com>
2022-06-29 16:46:58 -0700

    [add]modal and educate banner image

--- frontend/src/components/WalletModal.tsx
+++ frontend/src/components/WalletModal.tsx
@@ -1,48 +1,72 @@
-import React from "react";
-import { AiOutlineClose } from "react-icons/ai";
-import Button from "./Button";
-import cn from "classnames";
-
+import React from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
+import Button from './Button';
+import cn from 'classnames';
+import { useState } from 'react';
 const WalletModal = () => {
+  const [close, setClose] = useState(true);
   return (
-    <div
-      className={cn(
-        "w-[100%]",
-        "h-[100vh]",
-        "bg-[#000d]",
-        "flex",
-        "trasition-all",
-        " duration-100",
-        "ease-in-out",
-        "absolute",
-        "justify-center",
-        "items-center",
-        "top-0",
-        "opacity-0",
-        "left-0",
-        'mt-[-100vh]'
+    <>
+      {close ? (
+        <div
+          className={cn(
+            'w-[100%]',
+            'h-[100vh]',
+            'bg-[#000d]',
+            'flex',
+            'trasition-all',
+            ' duration-100',
+            'ease-in-out',
+            'absolute',
+            'justify-center',
+            'items-center',
+            'top-0',
+            'hidden',
+            'left-0',
+            'mt-[-100vh]',
+            'walletModal',
+          )}
+          style={{ display: '' }}
+          id="walletModal"
+        >
+          <div className="bg-slate-900 w-[90%] relative gap-3 flex flex-col justify-center min-h-[20vh] md:w-[50%]">
+            <div className="flex justify-center">
+              <h4
+                className="text-gray-300 cursor-pointer text-3xl absolute top-4 right-8"
+                onClick={() => setClose(false)}
+              >
+                X
+              </h4>
+              <h1 className="text-lg">
+                ZillPay Wallet is not installed.{' '}
+                <a
+                  href="https://chrome.google.com/webstore/detail/zilpay/klnaejjgbibmhlephnhpmaofohgkpgkd?hl=en"
+                  target="_blank"
+                >
+                  install now
+                </a>
+              </h1>
+            </div>
+
+            <div className="flex justify-center">
+              <Button>
+                <a
+                  className="block"
+                  href="https://zilpay.io/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Download Wallet
+                </a>
+              </Button>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      ) : (
+        <div>hel</div>
       )}
-      style={{ display: "" }}
-      id="walletModal"
-    >
-      <div className="bg-white w-[90%] gap-3 flex flex-col justify-center min-h-[50vh] md:w-[50%]">
-        <div className="flex justify-center">
-          <AiOutlineClose className="font-bold text-3xl text-red-600" />
-          <h1 className="text-lg font-bold">ZillPay Wallet is not installed</h1>
-        </div>
-        <div className="text-center w-[80%] mx-auto bg-slate-300 py-10">
-          SOME TEXT
-        </div>
-        <div className="flex justify-center">
-          <Button>
-            <a className="block" href="https://zilpay.io/" target="_blank" rel="noreferrer">
-              Download Wallet
-            </a>
-          </Button>
-        </div>
-        <div></div>
-      </div>
-    </div>
+    </>
   );
 };
 
--- frontend/src/pages/Educational.tsx
+++ frontend/src/pages/Educational.tsx
@@ -1,38 +1,45 @@
-import logo from "../../public/img/ZSBT-logos.jpeg";
+import logo from '../../public/img/ZSBT-logos.jpeg';
 
-import Link from "../components/Link";
-import AdaptiveContent from "../components/AdaptiveContent";
-import { AdaptiveContentProps } from "../types/types";
+import Link from '../components/Link';
+import AdaptiveContent from '../components/AdaptiveContent';
+import { AdaptiveContentProps } from '../types/types';
 
 const content: AdaptiveContentProps[] = [
   {
-    title: "The Set Up",
-    subtitle: "Setting up the Neo Savant IDE and Zilpay wallet",
+    title: 'The Set Up',
+    subtitle: 'Setting up the Neo Savant IDE and Zilpay wallet',
     content:
-      "Head to Neo Savant IDE. Create another account on your ZilPay. Request testnet $ZIL from faucet. Switch to testnet on your IDE.",
-    subcontent: " ",
+      'Head to Neo Savant IDE. Create another account on your ZilPay. Request testnet $ZIL from faucet. Switch to testnet on your IDE.',
+    subcontent: ' ',
   },
   {
-    title: "Scilla Language",
-    subtitle: "Writing your smart contract",
+    title: 'Scilla Language',
+    subtitle: 'Writing your smart contract',
     content:
-      "Create a new file by clicking the file icon on the upper left corner. Refer to the smart contracts in our github repository.(Zilliqa-Soulbound-Project/contract/example/proxy.scilla) modify it such that you can call the transition Earn_achievement in SBT_profile.",
-    subcontent: " ",
+      'Create a new file by clicking the file icon on the upper left corner. Refer to the smart contracts in our github repository.(Zilliqa-Soulbound-Project/contract/example/proxy.scilla) modify it such that you can call the transition Earn_achievement in SBT_profile.',
+    subcontent: ' ',
   },
 
   {
-    title: "Achievement Unlocked!",
-    subtitle: "Deploying the smart contract and earning the achievement",
+    title: 'Achievement Unlocked!',
+    subtitle: 'Deploying the smart contract and earning the achievement',
     content:
-      "After deploying the smart contract and calling the Earn_achievement transition, you have now earned your achievement! It should be displayed in your profile",
-    subcontent: " ",
+      'After deploying the smart contract and calling the Earn_achievement transition, you have now earned your achievement! It should be displayed in your profile',
+    subcontent: ' ',
   },
 ];
 
 export default function Educational() {
   return (
     <div className="text-white dark:text-white body-font">
-      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <div className="zsbt-logo">
+        <img
+          alt="logo"
+          src={logo}
+          className="object-cover object-center rounded"
+        />
+      </div>
+      <div className="container mx-auto px-5 py-24 md:flex-row flex items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium text-white dark:text-white">
             Deploy a smart contract and earn your achievement!
@@ -50,13 +57,6 @@ export default function Educational() {
           <div className="flex justify-center mt-3">
             <Link to="/profiles">View Profiles</Link>
           </div>
-        </div>
-        <div className=" rounded lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            alt="logo"
-            src={logo}
-            className="object-cover object-center rounded"
-          />
         </div>
       </div>
     </div>
